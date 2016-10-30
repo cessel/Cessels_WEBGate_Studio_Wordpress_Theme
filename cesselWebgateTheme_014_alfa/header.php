73b7094f1451d6c1d1553faed766e20938a9a7f8
cessel <cessel@yandex.ru>
2016-10-30 12:15:20 +0300

    Add yandex map script to header

--- cesselWebgateTheme_014_alfa/header.php
+++ cesselWebgateTheme_014_alfa/header.php
@@ -33,6 +33,26 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 <? wp_head(); ?>
+
+<script type="text/javascript">
+	ymaps.ready(init);
+	var myMap, 
+		myPlacemark;
+
+		function init()
+			{
+				var adress = '<? echo $contacts['adress']; ?>';
+	            ymaps.geocode(adress).then(function (res)
+					{
+						var position = res.geoObjects.get(0).geometry.getCoordinates();
+    					myMap = new ymaps.Map('map',{center: position,zoom : 14 });
+						var myPlacemark = new ymaps.Placemark(position,{hintContent: 'Русская Школа Искусств',balloonContent: 'Русская Школа Искусств - Москва, Ул. Ляпидевского, д. 10а'});
+						myMap.geoObjects.add(myPlacemark);  
+					}); 
+			
+			}	
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
