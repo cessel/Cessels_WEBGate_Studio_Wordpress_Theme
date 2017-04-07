c23cfab0acc0d84195f87c8e8bc9730f77d00ecf
cessel <cessel@yandex.ru>
2017-04-07 18:25:46 +0300

    Включен запрет скролла колесиком мыши для Яндеск.Карт

--- cesselWebgateTheme_014_alfa/header.php
+++ cesselWebgateTheme_014_alfa/header.php
@@ -50,7 +50,8 @@
 						var position = res.geoObjects.get(0).geometry.getCoordinates();
     					myMap = new ymaps.Map('map',{center: position,zoom : 14 });
 						var myPlacemark = new ymaps.Placemark(position,{hintContent: 'Русская Школа Искусств',balloonContent: 'Русская Школа Искусств - Москва, Ул. Ляпидевского, д. 10а'});
-						myMap.geoObjects.add(myPlacemark);  
+						myMap.geoObjects.add(myPlacemark);
+						myMap.behaviors.disable('scrollZoom');
 					}); 
 			
 			}	
